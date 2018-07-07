b507e0b2e447dee62da82d6303709e71b35ff226
Matthias Maurer <mmaurer@student.tugraz.at>
2018-07-07 18:20:34 +0200

    changed functionality to work with gamesituations

--- UnityCompetenceDemo/Assets/Scripts/GamesituationLoadingScript.cs
+++ UnityCompetenceDemo/Assets/Scripts/GamesituationLoadingScript.cs
@@ -27,9 +27,14 @@ public class GamesituationLoadingScript : MonoBehaviour {
             CompetenceComponent competenceComponent = CompetenceComponent.Instance;
             CompetenceComponentSettings settings = new CompetenceComponentSettings();
             settings.NumberOfLevels = maxLevel;
-            settings.SourceFile = "dataModel.xml";
+            settings.SourceFile = "dataModel2.xml";
             settings.LinearDecreasionOfCompetenceValuePerDay = 0.1f;
+            settings.CompetencePauseTimeInSeconds = 5;
+            settings.Phase = CompetenceComponentPhase.DEFAULT;
+            settings.ThreasholdRecommendationSelection = 1f / (60f * 60f * 60f); // very low, nearly zero
             competenceComponent.Settings = settings;
+
+
             
 
             //competenceComponent.Initialize();
@@ -45,13 +50,21 @@ public class GamesituationLoadingScript : MonoBehaviour {
         //in here: we fill the screen with content based on the current competence to learn/test
 
         //load current game situation
-        gsName = CompetenceComponent.Instance.GetCompetenceRecommendation();
+        gsName = CompetenceComponent.Instance.GetGamesituationRecommendation();
+        string message="";
+        if (gsName != null)
+        {
+            message += "Now the game presents a game situation which needs to be trained/tested next. ";
+            message += "In our case it is the game situation <b><i>'" + gsName + "'</i></b>. Based on the player's performance there is evidence for (success) or against (failure) ";
+            message += "the mastery of the game situation. Since this is a simple simulation the gameplay is simulated as a button click.";
+        }
+        else
+        {
+            message += "Right now there is no game situation which can be played - the player should pause the game. Click success or failure to request new game situation.";
+        }
 
 
 
-        string message = "Now the game presents a competence which needs to be trained/tested next. ";
-        message += "In our case it is the competence <b><i>'" + gsName + "'</i></b>. Based on the player's performance there is evidence for (success) or against (failure) ";
-        message += "the possession of this competence. Since this is a simple simulation the gameplay is simulated as a button click.";
         
 
         text.text = message;
@@ -61,21 +74,22 @@ public class GamesituationLoadingScript : MonoBehaviour {
         string txt = "";
         if (debugOutput)
         {
-            txt += "Competence values:\nid_value_timestamp\n\n";
+            txt += "Competence values:\nid_valueA_timestampA\nid_valueL_timestampL\n\n";
             CompetenceAssessmentObject cao = CompetenceComponent.Instance.getAssessmentObject();
             foreach (AssessmentCompetence ac in cao.competences)
             {
-                txt += ac.id + "_" + (Mathf.Round(100.0f * ac.value) / 100.0f).ToString() + "_" + ac.timestamp + "\n";
+                txt += ac.id + "_" + (Mathf.Round(100.0f * ac.valueAssessment) / 100.0f).ToString() + "_" + ac.timestampAssessment + "\n";
+                txt += ac.id + "_" + (Mathf.Round(100.0f * ac.valueLearning) / 100.0f).ToString() + "_" + ac.timestampLearning + "\n";
             }
             evaluationState.fontSize = 10;
         }
         else
         {
             txt += "Competences\nid: level\n\n";
-            Dictionary<string, int> competenceLevels = CompetenceComponent.Instance.getCompetenceLevels();
+            Dictionary<string, int[]> competenceLevels = CompetenceComponent.Instance.getCompetenceLevels();
             foreach (string key in competenceLevels.Keys)
             {
-                txt += key + ": " + competenceLevels[key].ToString() + "/"+ (maxLevel-1).ToString() + "\n";
+                txt += key + ": A" + competenceLevels[key][0].ToString() + " L" + competenceLevels[key][1].ToString() + "/" + (maxLevel-1).ToString() + "\n";
             }
             evaluationState.fontSize = 20;
         }
@@ -91,7 +105,8 @@ public class GamesituationLoadingScript : MonoBehaviour {
     public void UpdateCompetenceState(bool success)
     {
         //in here: the update of the current gamesituation is done based on the user input
-        CompetenceComponent.Instance.Update(gsName,success);
+        if(gsName!= null)
+            CompetenceComponent.Instance.UpdateGamesituation(gsName,success);
         LoadNextGamesituation();
     }
 
